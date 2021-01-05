d7567043f6406c520114b661373f9665225962c2
Stefan Sarow <stefan@pixelaffairs.com>
2021-01-05 22:04:02 +0100

    cru stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,44 @@
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
 
+const multer = require("multer");
+const jimp = require("jimp");
+const uuid = require("uuid");
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith("image/");
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "That filetype isn't allowed" }, false);
+    }
+  },
+};
 exports.homePage = (req, res) => {
   res.render("index");
 };
 
 exports.addStore = (req, res) => {
   res.render("editStore", { title: "Add store" });
+};
+
+exports.upload = multer(multerOptions).single("photo");
+
+exports.resize = async (req, res, next) => {
+  // check if there is no new file to resize
+  if (!req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split("/")[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // no we resize:
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
 };
 
 exports.createStore = async (req, res) => {
@@ -23,12 +55,19 @@ exports.getStores = async (req, res) => {
   res.render("stores", { title: "Stores", stores });
 };
 
+exports.getStore = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  res.render("store", { title: store.name, store });
+};
+
 exports.editStore = async (req, res) => {
   const store = await Store.findOne({ _id: req.params.id });
   res.render("editStore", { title: `Edit ${store.name}`, store });
 };
 
 exports.updateStore = async (req, res) => {
+  req.body.location.type = "Point";
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // return the updated store
     runValidators: true,
