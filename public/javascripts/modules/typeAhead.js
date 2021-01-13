2da9ff216a7c7f6fd6084056c61085216b2eac92
Stefan Sarow <stefan@pixelaffairs.com>
2021-01-13 12:13:22 +0100

    finished search

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -1,4 +1,5 @@
-const axios = require("axios");
+import axios from "axios";
+import dompurify from "dompurify";
 
 function searchResultsHTML(stores) {
   return stores
@@ -19,11 +20,53 @@ function typeAhead(search) {
       return;
     }
     searchResults.style.display = "block";
-    axios.get(`/api/v1/search?q=${this.value}`).then((res) => {
-      if (res.data.length) {
-        searchResults.innerHTML = searchResultsHTML(res.data);
-      }
-    });
+
+    axios
+      .get(`/api/v1/search?q=${this.value}`)
+      .then((res) => {
+        if (res.data.length) {
+          searchResults.innerHTML = dompurify.sanitize(
+            searchResultsHTML(res.data)
+          );
+          return;
+        }
+
+        searchResults.innerHTML = dompurify.sanitize(
+          `<div class="search__result">
+            No result for ${this.value} found!
+          </div>`
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+
+  searchInput.on("keyup", (e) => {
+    if (![38, 40, 13].includes(e.keyCode)) {
+      return;
+    }
+    const activeClass = "search__result--active";
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll(".search__result");
+    let next;
+
+    if (e.keyCode === 40 && current) {
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) {
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+      return;
+    }
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass);
   });
 }
 
